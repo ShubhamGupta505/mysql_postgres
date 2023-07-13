cc448b6588b122682794b938e2e34fe6614bf1ed
ShubhamGupta505 <shubhamgupta2133@gmail.com>
2023-07-13 18:55:46 +0530

    first commit

--- trial.py
+++ trial.py
@@ -61,30 +61,29 @@ if __name__ == "__main__":
         .getOrCreate()
 
     # df_emps = data_from_psql(spark)
-    df_emps = spark.read\
-        .format("jdbc")\
-        .option("url", "jdbc:mysql://192.168.64.1:3306/mydb")\
-        .option("dbtable", 'titanic')\
-        .option("user", "myuser")\
-        .option("password", "mypass")\
-        .option("driver", "com.mysql.jdbc.Driver") \
-        .load()
+    # df_emps = spark.read\
+    #     .format("jdbc")\
+    #     .option("url", "jdbc:mysql://192.168.144.1:3306/mydb")\
+    #     .option("dbtable", 'titanic')\
+    #     .option("user", "myuser")\
+    #     .option("password", "mypass")\
+    #     .option("driver", "com.mysql.jdbc.Driver") \
+    #     .load()
     # df_emps.show()
     # print(type(df_emps))
 
-    df = df_emps.toPandas() 
-    df = df.drop('SexCode', axis=1)
-    df = df.rename(columns = {'PClass': 'Passenger Class', 'Sex': 'Gender'})
-    # df = df.replace([0,1,'1st','2nd','3rd'],['Yes','No','First','Second','Third'])
+    # df = df_emps.toPandas() 
+    # df = df.drop('SexCode', axis=1)
+    # df = df.rename(columns = {'PClass': 'Passenger Class', 'Sex': 'Gender'})
+    # # df = df.replace([0,1,'1st','2nd','3rd'],['Yes','No','First','Second','Third'])
 
-    df_schema = StructType([StructField("name", StringType(), True) \
-        ,StructField("PClass", StringType(), True) \
-        ,StructField("Age", StringType(), True) \
-        ,StructField("Sex", StringType(), True) \
-        ,StructField("Survived", StringType(), True)])
 
-    pandas_to_pyspark = spark.createDataFrame(df, schema=df_schema)
-    pandas_to_pyspark.show()
+    # df_schema = StructType([StructField("name", StringType(), True) ,StructField("PClass", StringType(), True) ,StructField("Age", StringType(), True) ,StructField("Sex", StringType(), True) ,StructField("Survived", StringType(), True)])
+
+    # pandas_to_pyspark = spark.createDataFrame(df, schema=df_schema)
+    # pandas_to_pyspark.show()
+
+
 
     # df = df_emps.toPandas()                             --- convert PySpark DataFrame to Pandas
     # print(type(df))                                     --- <class 'pandas.core.frame.DataFrame'>
@@ -104,8 +103,9 @@ if __name__ == "__main__":
     #     .mode("overwrite")\
     #     .save()
 
-    pandas_to_pyspark.createOrReplaceTempView("empl")
-    sql_query = spark.sql("select * from empl")
+    # pandas_to_pyspark.createOrReplaceTempView("empl")
+    # df_emps.createOrReplaceTempView("empl")
+    # sql_query = spark.sql("select * from empl")
 
     # pandas_to_pyspark = spark.createDataFrame(df)         --- convert Pandas to PySpark DataFrame
     # print(type(pandas_to_pyspark))                        --- <class 'pyspark.sql.dataframe.DataFrame'>
@@ -115,10 +115,22 @@ if __name__ == "__main__":
 
 
     while True:
+        df_emps = spark.read\
+            .format("jdbc")\
+            .option("url", "jdbc:mysql://192.168.144.1:3306/mydb")\
+            .option("dbtable", 'titanic')\
+            .option("user", "myuser")\
+            .option("password", "mypass")\
+            .option("driver", "com.mysql.jdbc.Driver") \
+            .load()
+
+        df_emps.createOrReplaceTempView("empl")
+        sql_query = spark.sql("select * from empl")
+
         sql_query.write \
             .mode("overwrite") \
             .format("jdbc") \
-            .option("url", "jdbc:postgresql://192.168.64.1:5432/mydb") \
+            .option("url", "jdbc:postgresql://192.168.144.1:5432/mydb") \
             .option("dbtable", 'employee')\
             .option("user", 'myuser')\
             .option("password", 'mypass')\
